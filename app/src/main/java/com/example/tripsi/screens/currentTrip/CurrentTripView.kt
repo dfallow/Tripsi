c98e175774fcbfb028dc5b95a1cc0155f38d5ec5
David Fallow <davidfa@metropolia.fi>
2022-09-26 13:39:58 +0300

    buttons added

--- app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
+++ app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
@@ -1,16 +1,71 @@
 package com.example.tripsi.screens.currentTrip
 
+import android.util.Log
+import android.widget.Space
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.tripsi.R
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import kotlin.math.log
 
 @Composable
 fun CurrentTripView() {
-    ShowCurrentTripMap()
+
+    val viewModel = CurrentTripViewModel()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.7F)) {
+            ShowCurrentTripMap()
+        }
+
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceAround) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = viewModel.modifier,
+                shape = viewModel.shape
+            ) {
+                Text("Save the Moment", textAlign = TextAlign.Center)
+            }
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = viewModel.modifier,
+                shape = viewModel.shape
+            ) {
+                Text("Connect a Friend", textAlign = TextAlign.Center)
+            }
+        }
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = viewModel.modifier,
+            shape = viewModel.shape
+            ) {
+            Text("End Trip", textAlign = TextAlign.Center)
+        }
+    }
+
 }
 
 // Initial creation of the map
--- app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripViewModel.kt
+++ app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.tripsi.screens.currentTrip
 
-class CurrentTripViewModel {
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+
+class CurrentTripViewModel: ViewModel() {
+
+    val modifier = Modifier
+        .width(130.dp)
+
+    val shape = RoundedCornerShape(10.dp)
+
+
 }
