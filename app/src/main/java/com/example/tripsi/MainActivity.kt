223fba064f42bfd98f09cbca9c24e2e68bc55c2d
David Fallow <davidfa@metropolia.fi>
2022-09-26 11:06:48 +0300

    added dependencies and repositories

--- app/src/main/java/com/example/tripsi/MainActivity.kt
+++ app/src/main/java/com/example/tripsi/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.tripsi.screens.currentTrip.CurrentTripView
 import com.example.tripsi.ui.theme.TripsiTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CurrentTripView()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TripsiTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
+++ app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
@@ -1,2 +1,8 @@
 package com.example.tripsi.screens.currentTrip
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CurrentTripView() {
+
+}
