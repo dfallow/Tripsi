4c14bcc903134c7f7014eab40ae164c73f196339
David Fallow <davidfa@metropolia.fi>
2022-09-26 11:20:13 +0300

    initial map setup, before permissions

--- app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
+++ app/src/main/java/com/example/tripsi/screens/currentTrip/CurrentTripView.kt
@@ -1,8 +1,39 @@
 package com.example.tripsi.screens.currentTrip
 
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.tripsi.R
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
 
 @Composable
 fun CurrentTripView() {
+    ShowCurrentTripMap()
+}
 
+@Composable
+fun CurrentTripMap(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context).apply {
+            id = R.id.map
+        }
+    }
+    return mapView
+}
+
+@Composable
+fun ShowCurrentTripMap() {
+    val currentTripMap = CurrentTripMap()
+
+    var mapInitialized by remember(currentTripMap) { mutableStateOf(false)}
+
+    if (!mapInitialized) {
+        currentTripMap.setTileSource(TileSourceFactory.MAPNIK)
+        currentTripMap.controller.setZoom(9.0)
+        currentTripMap.controller.setCenter(GeoPoint(60.17, 24.95))
+    }
+    AndroidView({ currentTripMap })
 }
