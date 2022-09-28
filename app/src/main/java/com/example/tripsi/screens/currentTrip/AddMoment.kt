bd5ed40d04330767dc7a97df7ea48c240240161d
David Fallow <davidfa@metropolia.fi>
2022-09-28 12:37:30 +0300

    initial template complete

--- app/src/main/java/com/example/tripsi/screens/currentTrip/AddMoment.kt
+++ app/src/main/java/com/example/tripsi/screens/currentTrip/AddMoment.kt
@@ -1,21 +1,31 @@
 package com.example.tripsi.screens.currentTrip
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun AddMoment() {
     Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
     ) {
         MomentDetails()
+        MomentComment()
+        MomentPictures()
+        SaveOrDiscard()
     }
 }
 
@@ -58,6 +68,95 @@ fun MomentDetails() {
                 Text("Some Value", color = MaterialTheme.colors.onSecondary)
                 Text("Some Value", color = MaterialTheme.colors.onSecondary)
             }
+        }
+    }
+}
+
+@Composable
+fun MomentComment(
+    colors: TextFieldColors = myAppTextFieldColors()
+) {
+
+    var comment by remember { mutableStateOf("") }
+
+    TextField(
+        value = comment,
+        onValueChange = { comment = it },
+        shape = RoundedCornerShape(10.dp),
+        //textStyle = TextStyle(color = Color.Blue),
+        label = { Text("Describe the moment...")},
+        colors = colors,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.3f)
+            .padding(10.dp)
+            .border(BorderStroke(2.dp, Color.Black), shape = RoundedCornerShape(10.dp))
+    )
+}
+
+@Composable
+fun myAppTextFieldColors(
+    textColor: Color = Color.Black,
+    disabledTextColor: Color = Color.White,
+    backgroundColor: Color = Color.White,
+    cursorColor: Color = Color.White,
+    errorCursorColor: Color = Color.White,
+    focusedLabelColor: Color = Color.Black
+
+) = TextFieldDefaults.textFieldColors(
+    textColor = textColor,
+    disabledTextColor = disabledTextColor,
+    backgroundColor = backgroundColor,
+    cursorColor = cursorColor,
+    errorCursorColor = errorCursorColor,
+    focusedLabelColor = focusedLabelColor,
+)
+
+@Composable
+fun MomentPictures() {
+    Column() {
+
+        Row(
+            // Where the taken pictures will show
+        ) {
+
+        }
+
+        Button(
+            onClick = {
+            // TODO Take Picture and show in above row
+            }
+        ) {
+            Text("Take Picture")
+        }
+
+    }
+}
+
+@Composable
+fun SaveOrDiscard() {
+    // Contains the buttons for saving or discarding the moment
+    Row (
+        horizontalArrangement = Arrangement.SpaceAround,
+       verticalAlignment = Alignment.Bottom ,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(10.dp)
+            ) {
+        Button(
+            onClick = {
+            /*TODO*/
+            }
+        ) {
+            Text("Save")
+        }
+        Button(
+            onClick = {
+                /*TODO*/
+            }
+        ) {
+            Text("Discard")
         }
     }
 }
