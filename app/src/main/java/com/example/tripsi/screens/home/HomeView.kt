bdbe7eb4b53fead53298bfc1730ce0725bbcfab4
Ali-k-fahad <55328660+Ali-k-fahad@users.noreply.github.com>
2022-10-03 19:11:01 +0300

    Start trip
    if there is a trip ongoing or planned, then (plan a trip ) button will be disabled.

--- app/src/main/java/com/example/tripsi/screens/home/HomeView.kt
+++ app/src/main/java/com/example/tripsi/screens/home/HomeView.kt
@@ -1,18 +1,19 @@
 package com.example.tripsi.screens.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.airbnb.lottie.compose.*
 import com.example.tripsi.R
@@ -27,14 +28,26 @@ fun HomeView(navController: NavController, tripDbViewModel: TripDbViewModel) {
     val homeViewModel = HomeViewModel()
 
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.globe))
-    val progress by animateLottieCompositionAsState(composition = composition, iterations = LottieConstants.IterateForever)
-    
-    Column(
-                        modifier = Modifier . fillMaxSize (),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+    val upcomingTrips =
+        tripDbViewModel.getAllTripsDataByStatus(TripStatus.UPCOMING.status)
+            .observeAsState()
+    var trip: Trip? = null
 
-                ) {
+    if (upcomingTrips.value != null) {
+        if (upcomingTrips.value!!.isNotEmpty()) {
+            trip = upcomingTrips.value!![0].trip
+        }
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
         LottieAnimation(
             composition = composition,
             progress = progress,
@@ -42,74 +55,104 @@ fun HomeView(navController: NavController, tripDbViewModel: TripDbViewModel) {
                 .clip(shape = RoundedCornerShape(16.dp))
                 .size(500.dp, 250.dp)
         )
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.Top,
-                        horizontalArrangement = Arrangement.End){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.End
+        ) {
 
-                        Button(
-                            onClick = {
-                                navController.navigate(Screen.TravelsScreen.route)
-                            },
-                            shape = RoundedCornerShape(
-                                topStartPercent = 25,
-                                bottomStartPercent = 25
-                            ),
-                            contentPadding = PaddingValues(
-                                start = 30.dp,
-                                top = 15.dp,
-                                end = 30.dp,
-                                bottom = 15.dp
-                            )
-                        ) {
-                            Text(text = "Trip history")
-                        }
-                    }
+            Button(
+                onClick = {
+                    navController.navigate(Screen.TravelsScreen.route)
+                },
+                shape = RoundedCornerShape(
+                    topStartPercent = 25,
+                    bottomStartPercent = 25
+                ),
+                contentPadding = PaddingValues(
+                    start = 30.dp,
+                    top = 15.dp,
+                    end = 30.dp,
+                    bottom = 15.dp
+                )
+            ) {
+                Text(text = "Trip history")
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            if (trip != null) {
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.PlanScreen.route)
+                    },
+                    enabled = false,
+                    shape = RoundedCornerShape(
+                        topEndPercent = 25,
+                        bottomEndPercent = 25
+                    ),
+                    contentPadding = PaddingValues(
+                        start = 30.dp,
+                        top = 15.dp,
+                        end = 30.dp,
+                        bottom = 15.dp
+                    )
+                ) {
+                    Text(text = "Plan a trip")
+                }
+            } else {
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.PlanScreen.route)
+                    },
+                    shape = RoundedCornerShape(
+                        topEndPercent = 25,
+                        bottomEndPercent = 25
+                    ),
+                    contentPadding = PaddingValues(
+                        start = 30.dp,
+                        top = 15.dp,
+                        end = 30.dp,
+                        bottom = 15.dp
+                    )
+                ) {
+                    Text(text = "Plan a trip")
+                }
+            }
+        }
 
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.Top,
-                        horizontalArrangement = Arrangement.Start) {
-                        //check for upcoming trips
-                        val upcomingTrips =
-                            tripDbViewModel.getAllTripsDataByStatus(TripStatus.UPCOMING.status)
-                                .observeAsState()
-                        var trip: Trip? = null
+        //check for upcoming trips
+        if (trip == null) {
+        } else {
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 20.dp)
+                .clip(shape = RoundedCornerShape(
+                    topEndPercent = 10,
+                    topStartPercent = 10
+                )
+                )
+                .background(color = Color(0xFF3C493F)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
 
-                        if (upcomingTrips.value != null) {
-                            if (upcomingTrips.value!!.isNotEmpty()) {
-                                trip = upcomingTrips.value!![0].trip
-                            }
-                        }
-
-                        if (trip != null) {
-                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                                Text("Your trip to ${trip.destination} is coming up.")
-                                Text("Ready to start?")
-                                Button(onClick = {
-                                    tripDbViewModel.tripId = trip.tripId
-                                    navController.navigate(Screen.CurrentScreen.route)
-                                }) {
-                                    Text(text = "start a trip")
-                                }
-                            }
-                        } else {
-                            Button(onClick = {
-                                navController.navigate(Screen.PlanScreen.route)
-                            },
-                                shape = RoundedCornerShape(
-                                    topEndPercent = 25,
-                                    bottomEndPercent = 25
-                                ),
-                                contentPadding = PaddingValues(
-                                    start = 30.dp,
-                                    top = 15.dp,
-                                    end = 30.dp,
-                                    bottom = 15.dp
-                                )
-                                ) {
-                                Text(text = "Plan a trip")
-                            }
-                        }
-                    }
+            ){
+                Text("Your trip to ${trip.destination} is coming up.", color = Color.White,fontSize = 20.sp)
+                Text("Ready to start?", color = Color.White, fontSize = 20.sp)
+                Button(onClick = {
+                    tripDbViewModel.tripId = trip.tripId
+                    navController.navigate(Screen.CurrentScreen.route)
+                },
+                        shape = RoundedCornerShape(
+                            25
+                        )
+                ) {
+                    Text(text = "start a trip")
+                }
+            }
+        }
     }
-
 }
