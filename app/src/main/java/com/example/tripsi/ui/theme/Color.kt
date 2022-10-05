6b89fd7b273d2ce2a586bd22a521ec8959ce9dd7
David Fallow <55578874+dfallow@users.noreply.github.com>
2022-10-05 12:23:27 +0300

    Revert "Revert "Bottom nav bar""

--- app/src/main/java/com/example/tripsi/ui/theme/Color.kt
+++ app/src/main/java/com/example/tripsi/ui/theme/Color.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.graphics.Color
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
+
+val darkGreen = Color(0xFF3C493F)
+
 val Teal200 = Color(0xFF03DAC5)
 // TODO Add dark counterparts
 
@@ -13,4 +16,5 @@ val genericBtnTextLight = Color(0xFF2D0320)
 val cancelBtnLight = Color(0xFF2D0320)
 val cancelBtnTextLight = Color(0xFFFFFFFF)
 val navBar = Color(0xFF3C493F)
+
 
