cfe545530ac9dbb48268f7613be8f7f4630ec6df
David Fallow <davidfa@metropolia.fi>
2022-10-13 18:42:49 +0300

    different user icons added
    location now checks whether to use gps or network

--- app/src/main/java/com/example/tripsi/utils/Location.kt
+++ app/src/main/java/com/example/tripsi/utils/Location.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.text.BoringLayout
 import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +15,9 @@ import org.osmdroid.util.GeoPoint
 class Location(context: Context): LocationListener {
 
     var userLocation by mutableStateOf(GeoPoint(0.0,0.0))
+    private var isGPSEnabled = false
+    private var isNetworkEnabled = false
+    private lateinit var startLocation: Location
     private val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
     override fun onLocationChanged(p0: Location) {
@@ -23,9 +27,28 @@ class Location(context: Context): LocationListener {
 
     @SuppressLint("MissingPermission")
     fun startUpdatingLocation() {
-        val startLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+        try {
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // No network information
+                Log.d("error", "Location is not available")
+                return
+            } else {
+                startLocation = if (isGPSEnabled) {
+                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)!!
+                } else {
+                    locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)!!
+                }
+            }
+        } catch (e : Exception) {
+            Log.d("Error",e.message.toString())
+        }
+
+
         userLocation = GeoPoint(startLocation.latitude, startLocation.longitude)
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, this)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
         Log.d("Location", startLocation.toString())
     }
 
